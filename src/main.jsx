37e6112b603aeea1913d5ddd9241044cad239a5f
imkawe <zebimkawed@gmail.com>
2025-04-10 01:38:20 -0500

    Initiafl colmm1

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
-// src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
 import About from './pages/About';
